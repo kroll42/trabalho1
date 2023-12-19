e80cb083d7f4896b8a3400be7bdca685abb004a6
Karolyne Muniz <106029568+kroll42@users.noreply.github.com>
2023-12-19 02:38:56 -0300

    Update KinematicsResultPanel.java

--- KinematicsResultPanel.java
+++ KinematicsResultPanel.java
@@ -9,11 +9,37 @@ public class KinematicsResultPanel extends JPanel {
         add(resultLabel);
     }
 
-    public void displayResult(double result) {
-        resultLabel.setText("Resultado: " + result);
+    public void displayResult(double result, String unit) {
+        String formattedResult = formatResult(result, unit);
+        resultLabel.setText("Resultado: " + formattedResult);
     }
 
     public void displayError(String errorMessage) {
         resultLabel.setText("Erro: " + errorMessage);
+    }
+
+    private String formatResult(double result, String unit) {
+        switch (unit) {
+            case "m/s":
+                return String.format("%.2f m/s", result);
+            case "km/h":
+                return String.format("%.2f km/h", result);
+            case "ft/s":
+                return String.format("%.2f ft/s", result);
+            case "mi/h":
+                return String.format("%.2f mi/h", result);
+            case "graus":
+                return String.format("%.2f graus", result);
+            case "radianos":
+                return String.format("%.2f radianos", result);
+            case "segundos":
+            	return String.format("%.2f segundos", result);
+            case "minutos":
+            	return String.format("%.2f minutos", result);
+            case "horas":
+            	return String.format("%.2f horas", result);
+            default:
+                return Double.toString(result); // Se a unidade não for reconhecida, mostrar apenas o número
+        }
     }
 }
